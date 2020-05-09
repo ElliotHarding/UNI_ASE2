4f33c4e17393588a84111a13cc124eb75418c375
elliotharding <elliot.one.harding@gmail.com>
2020-05-09 21:42:21 +0100

    Flood fill improvemetns

--- floodFill.hs
+++ floodFill.hs
@@ -5,7 +5,7 @@ grid :: Int -> a -> Array(Int, Int) a
 grid size value = array ((0,0),(size-1,size-1)) [((x,y),value) | x<-[0..size-1], y<-[0..size-1]]
 
 printGrid :: Show a => Array (Int, Int) a ->  IO [()]
-printGrid =  mapM (putStrLn . textRepresentation) . toSimpleArray
+printGrid =  mapM (putStrLn . intercalate " " . map show) . toSimpleArray
 
 toSimpleArray :: Array (Int, Int) a -> [[a]]
 toSimpleArray grid = [[grid ! (x, y) | x<-[lowx..highx]] |  y<-[lowy..highy]] 
@@ -15,9 +15,8 @@ toComplexArray :: [[a]] -> Array(Int, Int) a
 toComplexArray grid = array ((0,0),((length $ grid !! 0) - 1,(length grid) - 1))  entries  
   where entries = concatMap (\z -> map (\y -> ((fst y, fst z), snd y))  (snd z)) $ zip [0..] $ map (\x -> zip [0..] x) grid
 
---Do some formatting so it looks nicer later on
-textRepresentation :: Show a => [a] -> String
-textRepresentation =  intercalate " " . map show
+addSpaces :: [Char] -> [Char]
+addSpaces inString = inString ++ replicate (5 - length inString) '.'
 
 inBounds :: Array (Int, Int) String -> (Int, Int) -> Bool
 inBounds grid (x, y) = x >= lowx && x <= highx && y >= lowy && y <= highy
@@ -39,5 +38,3 @@ floodFill grid point@(x, y) target replacement =
 
 main = do
 	printGrid $ floodFill (toComplexArray [["White", "White", "White", "Blue", "Blue"], ["Blue", "White", "Blue", "Blue", "Blue"], ["Blue", "Blue", "Blue", "Green", "Green"], ["Green", "Red", "Blue", "Black", "Black"], ["Blue", "Blue", "Blue", "Green", "Blue"]]) (1,2) "Blue" "Red"
-	--putStrLn addSpaces "hello"
-	--putStrLn (replicate 5 3)
